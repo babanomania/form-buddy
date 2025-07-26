0210c36a6252d1f81d08e098d5c5a4a7d0b72a01
Shouvik Basu <babanomania@gmail.com>
2025-07-26 21:45:51 +1000

    feat(ui): switch to material ui light theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
 import './index.css'
 import App from './App.tsx'
 
@@ -11,8 +12,17 @@ if (import.meta.env.PROD && 'serviceWorker' in navigator) {
   })
 }
 
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+})
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
